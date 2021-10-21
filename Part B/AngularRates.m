3ab6e01249c6ac2b01cf1ae5cb3b0406996ef372
reynobody <54421000+reynobody@users.noreply.github.com>
2021-10-21 17:00:52 +1100

    Add files via upload

--- Part B/AngularRates.m
+++ Part B/AngularRates.m
@@ -1,48 +1,74 @@
 % Function to estimate alpha_dot and beta_dot
 
-function ret = AngularRates(cg)
+function ret = AngularRates(X,Xdot)
 
-if cg == 1
-    [ FlightData ] = aero3560_LoadFlightDataPC9_nominalCG1();
-elseif cg == 2
-    [ FlightData ] = aero3560_LoadFlightDataPC9_CG2();
-end
+% Extract body accelerations components
+udot = Xdot(1);
+vdot = Xdot(2);
+wdot = Xdot(3);
 
-u1 = ;
-rho = 1.225;
-Q = 0.5*rho*u1^2;
+% Extract body velocities components
+u = X(1);
+v = X(2);
+w = X(3);
 
-Zu = ;
-Za = ;
-Du = ;
-Da = ;
-Dq = ;
-Zdt = ;
-Zde = ;
-Ddt = ;
-Dde = ;
+% Calculate V
+V = sqrt(u^2+v^2+w^2);
+Vdot = (u*udot+v*vdot+w*wdot)/V;
 
-alphadot = Zu/u1*Du + Za/u1*Da + Dq;
+% Calculate total alpha dot (alpha rate of change)
+alphadot = (u*wdot-w*udot)/(u^2+w^2);
 
-Yb = Q*FlightData.Geo.S*FlightData.Aero.Cyb/FlightData.Inertial.m;
-Yp = Q*FlightData.Geo.S*FlightData.Geo.b*FlightData.Aero.Cyp/...
-    (2*FlightData.Inertial.m*u1);
-Yr = Q*FlightData.Geo.S*FlightData.Geo.b*FlightData.Aero.Cyr/...
-    (2*FlightData.Inertial.m*u1);
-theta1 = ;
-Db = ;
-Dp = ;
-Dr = ;
-Dphi = ;
-Dpsi = ;
-Ydr = Q*FlightData.Geo.S*FlightData.Geo.b*FlightData.Aero.Cydr;
-Dda = ;
-Ddr = ;
+% Calculate total beta dot (beta rate of change)
+betadot = (vdot/V - v*Vdot/V^2)/sqrt(1-v^2/V^2);
 
-betadot = Yb/u1*Db + Yp/u1*Dp + (Yr/u1-1)*Dr + g*cos(theta1)/u1*Dphi + ...
-    Ydr/u1*Ddr;
-
+% Form an output vector
 ret.alphadot = alphadot;
 ret.betadot = betadot;
 
 end
+
+
+% 
+% if cg == 1
+%     [ FlightData ] = aero3560_LoadFlightDataPC9_nominalCG1();
+% elseif cg == 2
+%     [ FlightData ] = aero3560_LoadFlightDataPC9_CG2();
+% end
+% 
+% u1 = ;
+% rho = 1.225;
+% Q = 0.5*rho*u1^2;
+% 
+% Zu = ;
+% Za = ;
+% Du = ;
+% Da = ;
+% Dq = ;
+% Zdt = ;
+% Zde = ;
+% Ddt = ;
+% Dde = ;
+% 
+% alphadot = Zu/u1*Du + Za/u1*Da + Dq;
+% 
+% Yb = Q*FlightData.Geo.S*FlightData.Aero.Cyb/FlightData.Inertial.m;
+% Yp = Q*FlightData.Geo.S*FlightData.Geo.b*FlightData.Aero.Cyp/...
+%     (2*FlightData.Inertial.m*u1);
+% Yr = Q*FlightData.Geo.S*FlightData.Geo.b*FlightData.Aero.Cyr/...
+%     (2*FlightData.Inertial.m*u1);
+% theta1 = ;
+% Db = ;
+% Dp = ;
+% Dr = ;
+% Dphi = ;
+% Dpsi = ;
+% Ydr = Q*FlightData.Geo.S*FlightData.Geo.b*FlightData.Aero.Cydr;
+% Dda = ;
+% Ddr = ;
+% 
+% betadot = Yb/u1*Db + Yp/u1*Dp + (Yr/u1-1)*Dr + g*cos(theta1)/u1*Dphi + ...
+%     Ydr/u1*Ddr;
+% 
+% ret.alphadot = alphadot;
+% ret.betadot = betadot;
