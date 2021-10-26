46ec3a36e364a36c033fa911cd910d703d12555b
reynobody <54421000+reynobody@users.noreply.github.com>
2021-10-26 15:15:35 +1100

    Add files via upload

---
+++ Part B/Controls.m
@@ -0,0 +1,42 @@
+% Control function for FM assignment 3 
+% Outputs control functions and timestep information
+% - X_dt
+% - Y_dt
+% - X_de
+% - Y_de
+% - X_da
+% - Y_da
+% - X_dr
+% - Y_dr
+% - X_df
+% - Y_df
+% - SimTime (simulated amount of time in seconds)
+% - TimeStep (time step in seconds)
+% - freq (frequency of outputs)
+
+function output = Controls(casenum)
+
+if casenum == 1
+    filename = 'Control_Data1';
+elseif casenum == 2
+    filename = 'Control_Data2';
+elseif casenum == 3
+    filename = 'Control_Data3';
+elseif casenum == 4
+    filename = 'Control_Data4';
+elseif casenum == 5
+    filename = 'Control_Data5';
+elseif casenum == 6
+    filename = 'Control_Data6';
+elseif casenum == 7
+    filename = 'Control_Data7';
+elseif casenum == 8
+    filename = 'Control_Data8';
+end
+
+load_data = load(filename);
+output = load_data.GUI_save;
+
+end
+
+
