bc5da58648d8e8ddc979cb3c92578ee9da1c918a
dowu6071 <92772188+dowu6071@users.noreply.github.com>
2021-10-28 18:26:21 +1100

    Update main.m

--- Part B/main.m
+++ Part B/main.m
@@ -5,7 +5,6 @@ clf reset
 clc
 
 condition = 1;
-h = 1000*0.3048; % m
 
 % initialisation-----------------------------------------------------------
 [FlightData,IC,V] = Initialisation(condition);
@@ -33,7 +32,7 @@ delta_a = ControlInputs.Y_da;
 time_r = ControlInputs.X_dr;
 delta_r = ControlInputs.Y_dr; 
 
-U0_GUI = [delta_e'; delta_t'; delta_a'; delta_r'];
+U_GUI = [delta_e'; delta_t'; delta_a'; delta_r'];
 
 % Not sure if the stopping and stepping time are fixed with ControlGUI 
 % Probs not, ill still leave this here tho we can delete later
@@ -42,7 +41,7 @@ timestep = ControlInputs.TimeStep;
 
 count = 0;
 count_c = 0;
-for time=1:timestep:timestop
+while time=0:timestep:timestop
     
     % Update counter
     count = count+1;
@@ -54,22 +53,23 @@ for time=1:timestep:timestop
         count_c = count_c + 1;
         
         % Get control input from the GUI
-        U0 = U0_GUI(:,count_c);
+        U = U_GUI(:,count_c);
         
         % Integrate the trajectory at each time step
-        X_new = Integrate(X0,FlightData,h,U0,dt);
+        X_new = Integrate(X0,FlightData,h,U,dt);
         
     else
         % Integrate without control input
-        X_new = Integrate(data,FC,dt);
+        X_new = Integrate(X0,FlightData,h,U0,dt);
     end
     
     % Update states and rates
     X0 = X_new;
 
-%     % IM NOT SURE ABOUT THESE FOLLOWING LINES, WHAT THEY MEAN
-%     % Form the state matrix for each time step
-%     X_mat(:,count) = X0; 
-%     
-%     % Form the control matrix for each time step
-%     control(:,count)=FC.U0;
+     % IM NOT SURE ABOUT THESE FOLLOWING LINES, WHAT THEY MEAN
+    % Form the state matrix for each time step
+     X_mat(:,count) = X0; 
+     
+     % Form the control matrix for each time step
+     control(:,count)=U0;
+end
