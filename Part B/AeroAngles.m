1d1d97863de8e2a45eadc5185b9b8cc525fe41e3
dowu6071 <92772188+dowu6071@users.noreply.github.com>
2021-10-30 00:44:18 +1100

    Update AeroAngles.m

--- Part B/AeroAngles.m
+++ Part B/AeroAngles.m
@@ -3,7 +3,10 @@
 % Compute V, alpha and beta using current body velocities u,v and w
 % gives the values in m/s and in radians for the angles
 
-function [V alpha beta] = AeroAngles(u,v,w)
+function [V alpha beta] = AeroAngles(X)
+u = X(1);
+v = X(2);
+w = X(3);
 
 V = sqrt(u^2+v^2+w^2);
 alpha = atan(w/u);
